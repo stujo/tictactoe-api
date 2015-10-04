7c3a758c18ea7ec5d7b367fffb1b94bb67538f0c
Stujo <stujo@github.com>
2015-10-03 21:40:16 -0700

    Express App to Serve API requests at /api/v1/---------/X

---
+++ app.js
@@ -0,0 +1,62 @@
+var TicTacToe = require('tictactoe-agent');
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+
+function validGame(game) {
+    if (!game || game.length != 9) {
+        return false;
+    } else {
+        for (var i = 0; i < game.length; i++) {
+            var char = game.charAt(i);
+            if (char != '-' && char != 'X' && char != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+app.get('/api/v1/:game/:player', function(req, res) {
+
+    var game = req.params.game;
+    var player = req.params.player;
+
+    if (player != 'X' && player != 'O') {
+        res.status(404).json({
+            error: 'player must be X or O'
+        });
+    } else if (!validGame(game)) {
+        res.status(404).json({
+            error: 'game must be nine X,O or -'
+        });
+    } else {
+        var model = new TicTacToe.Model(game, player);
+
+        try {
+            var summary = model.boardSummary();
+
+            res.json({
+                game: game,
+                player: player,
+                score: summary.score,
+                recommendation: summary.recommendation.index,
+                strength: summary.recommendation.score
+            });
+        } catch (err) {
+            res.status(500).json({
+                error: err.message
+            });
+        }
+    }
+});
+
+var server = app.listen(app.get('port'), function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
