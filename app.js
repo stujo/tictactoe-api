cf6b0041191913ecc517476011724fa31577d936
Stujo <stujo@github.com>
2015-10-03 22:01:29 -0700

    Enable CORS

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 function validGame(game) {
     if (!game || game.length != 9) {
